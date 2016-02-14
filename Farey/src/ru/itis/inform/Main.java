7fad2a541916f11431df47507e7e3e1a48622d46
Kamil Karimov <imanymuch@gmail.com>
2016-02-14 22:59:08 +0300

    Revert "second"
    This reverts commit c33794e55a71e6b266d3105045f3f96d58189dac.

--- Farey/src/ru/itis/inform/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.itis.inform;
-
-public class Main {
-    public static void main(String[] args) {
-        FareySequenceGeneratorArrayImpl sequence = new FareySequenceGeneratorArrayImpl();
-        sequence.generate();
-        sequence.show();
-    }
-}
