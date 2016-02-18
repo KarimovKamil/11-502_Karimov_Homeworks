e4dd6bb142684c36cc7148ac7e72ac9858777cba
Kamil Karimov <imanymuch@gmail.com>
2016-02-18 22:53:13 +0300

    Delete

--- FareyLinkedList/src/ru/itis/inform/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.itis.inform;
-
-public class Main {
-    public static void main(String[] args) {
-        FareySequenceGenerator sequence = new FareySequenceGenerator(5);
-        sequence.generate();
-        sequence.show();
-    }
-}
